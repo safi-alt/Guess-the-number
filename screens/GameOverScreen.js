0d4c35525485894d6f18ad5c438b79f080fadcf3
safi <safiazim8@gmail.com>
2021-01-08 00:18:58 +0500

    Closer look at Text Component

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, Button, Image } from "react-native";
 import BodyText from "./../components/BodyText";
 import TitleText from "./../components/TitleText";
+import Colors from "../constants/Colors";
 
 const GameOverScreen = (props) => {
   return (
@@ -9,18 +10,25 @@ const GameOverScreen = (props) => {
       <TitleText>The Game is Over!!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          fadeDuration={1000}
-          //source={require("../assets/success.png")}
-          source={{
-            uri:
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR952h7H7Jh3TlOWLbrs9uNop0tEK1v0j8USw&usqp=CAU",
-          }}
+          //fadeDuration={1000}
+          source={require("../assets/success.png")}
+          // source={{
+          //   uri:
+          //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR952h7H7Jh3TlOWLbrs9uNop0tEK1v0j8USw&usqp=CAU",
+          // }}
           style={styles.image}
           resizeMode="cover"
         />
       </View>
-      <BodyText>Number of Rounds: {props.roundsNumber}</BodyText>
-      <BodyText>Number was: {props.userNumber}</BodyText>
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>.
+        </BodyText>
+      </View>
+
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -44,6 +52,18 @@ const styles = StyleSheet.create({
     borderColor: "black",
     overflow: "hidden",
     marginVertical: 30,
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: "open-sans-bold",
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
   },
 });
 
